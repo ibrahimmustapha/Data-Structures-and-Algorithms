39ee9063a02cfa18e196ef85107a06eac9c786d9
ibrahimmustapha <musib581@gmail.com>
2022-04-16 11:10:35 +0000

    indexOf and contains method

--- src/com/codewithibrahim/dsa/Main.java
+++ src/com/codewithibrahim/dsa/Main.java
@@ -11,5 +11,6 @@ public class Main {
         list.addLast(10);
         list.addLast(20);
         list.addLast(30);
+        System.out.println(list.contains(11));
     }
 }
--- src/com/codewithibrahim/dsa/linkedlist/MyLinkedList.java
+++ src/com/codewithibrahim/dsa/linkedlist/MyLinkedList.java
@@ -18,7 +18,7 @@ public class MyLinkedList {
     public void addLast(int item) {
         Node node = new Node(item);
 
-        if (first == null)
+        if (isEmpty())
             first = last = node;
         else {
             last.next = node;
@@ -27,11 +27,36 @@ public class MyLinkedList {
     }
 
     public void addFirst(int item) {
+        Node node = new Node(item);
 
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            node.next = first;
+            first = node;
+        }
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item) return index;
+                current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
     }
 
     // deleteFirst
     // deleteLast
-    // contains
     // indexOf
 }
